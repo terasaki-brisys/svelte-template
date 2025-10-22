bd2e31d89260b1544057ab1f73920d8965464b19
terasaki <terasaki@brisys.co.jp>
2025-10-22 10:41:10 +0900

    fix svelte type error

--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -1,2 +1,8 @@
+/// <reference types="svelte" />
 /// <reference types="vite/client" />
 /// <reference types="@sveltejs/kit" />
+
+// 念押し：.svelte をモジュールとして扱う宣言
+declare module '*.svelte' {
+  export { SvelteComponent as default } from 'svelte';
+}
